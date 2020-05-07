06f3d43500bdf27bea2ec4ecfb5009f3e83079e2
Ian Kennedy <ikennedy@uw.edu>
2020-05-07 14:17:11 -0700

    added homework 2 format

--- sim.R
+++ sim.R
@@ -1,8 +1,8 @@
 #Set the intercept
 b0 <- 2
 #Set phi
-rho <- .16
-rho_2 <- -.45
+rho <- .5
+rho_2 <- -.4
 #Set the number of periods
 n <- 50
 t <- seq(0,n)
@@ -10,17 +10,25 @@ y1 <- rep(0,n)
 for (i in 3:length(t)){
 y1[i] <- mean(y1) + (y1[i-1]-mean(y1))*rho + (y1[i-2]-mean(y1))*rho_2 + rnorm(1,0,15)
 }
-plot(y1,type="l", col="red",ylab="y",xlab="Time")
-acf(y1)
-pacf(y1)
-for (i in 3:length(t)){
-y2[i] <- y2[i-1]*phi + y2[i-2]*phi_2 + b1*t[i] + b0 + rnorm(1,0,15)
-}
-plot(y2,type="l", col="red",ylab="y",xlab="Time")
 
-par(mfrow=c(2:1))
+for (i in 3:length(t)){
+  y1[i] <- mean(y1) + (y1[i-1]-mean(y1))*rho + rnorm(1,0,15)
+}
 acf(y1)
-acf(y2)
-par(mfrow=c(2:1))
 pacf(y1)
+#Set the intercept
+b0 <- 0
+#Set phi
+phi <- .4
+phi_2 <- -.1
+n <- 50
+t <- seq(0,n)
+y2 <- rep(0,n)
+
+for (i in 3:length(t)){
+y2[i] <- y2[i-1]*phi + y2[i-2]*phi_2 + b0 + rnorm(1,0,15)
+}
+
+acf(y2)
+
 pacf(y2)
